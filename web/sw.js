6a7eff149194f93eac3588d293e3fe396091475a
2046 <u@uuu.fo>
2025-08-04 18:51:42 +0800

    chore: cache fonts using service worker

--- web/sw.js
+++ web/sw.js
@@ -17,4 +17,23 @@ self.addEventListener('fetch', function (event) {
       })
     }))
   }
+
+  if (event.request.destination === 'font') {
+    event.respondWith(new Promise(function (resolve, reject) {
+      caches.match(event.request).then(function (responseFromCache) {
+        if (responseFromCache) {
+          resolve(responseFromCache)
+        } else {
+          fetch(event.request).then(function (responseFromNetwork) {
+            caches.open('fonts-v1').then(function (cache) {
+              cache.put(event.request, responseFromNetwork.clone())
+              resolve(responseFromNetwork)
+            })
+          }).catch((error) => {
+            reject(error)
+          })
+        }
+      })
+    }))
+  }
 })
